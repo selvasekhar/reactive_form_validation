f34024ff05fae5614ac1acccaa58f10d721aba0b
selvasekhar <selvasekhar@gmail.com>
2022-11-03 21:21:20 +0530

    reactive forms validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators, FormsModule } from '@angular/forms'
+import {NgModel} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-project2';
+  constructor(private fb: FormBuilder) { }
+
+  registrationForm = this.fb.group({
+    userName: ['selvasekar', [Validators.required]],
+    password: [''],
+    confirmpassword: [''],
+   address: this.fb.group({
+      city: [''],
+      state: [''],
+      ps: [''],
+    })
+  })
+  // title = 'my-first-project2';
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('Selvasekar'),
+  //   password: new FormControl(''),
+  //   confirmpassword: new FormControl(''),
+  //  address: new FormGroup({
+  //     city: new FormControl(),
+  //     state: new FormControl(),
+  //     ps: new FormControl(),
+  //   })
+  // });
+
+  // loadAPIData() {
+  //   this.registrationForm.setValue({
+  //     userName: 'Selva',
+  //     password: 'test',
+  //     confirmpassword: 'test',
+
+  //     address: {
+  //       city: 'City',
+  //       state: 'State',
+  //       ps: '123456',
+  //     }
+  //   });
+  // }
 }
+
